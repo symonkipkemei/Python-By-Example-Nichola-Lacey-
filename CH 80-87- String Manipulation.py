924a884a080655033c07f4ad74bfb7da04346a2d
symonkipkemei <symonkipkemei@gmail.com>
2021-07-17 16:26:09 +0300

    String Manipulation

---
+++ CH 80-87- String Manipulation.py
@@ -0,0 +1,16 @@
+'''
+080
+Ask the user to enter their
+first name and then display
+the length of their first name.
+Then ask for their surname
+and display the length of
+their surname. Join their first
+name and surname together
+with a space between and
+display the result. Finally,
+display the length of their full
+name (including the space).
+
+'''
+
