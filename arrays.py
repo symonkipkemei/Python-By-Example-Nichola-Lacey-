8c0a1d52feea74ddeabe636f9d68e638e098e7c0
symonkipkemei <symonkipkemei@gmail.com>
2021-07-18 12:22:44 +0300

    arrays

---
+++ arrays.py
@@ -0,0 +1,17 @@
+from array import *
+# when storing numbers in arrays unlike lists and dictionaries,
+# you need to import the array first
+
+
+num = ["i", 345, 678, 987, 6768]
+# above is a list with more than one data type
+print(num)
+# when declaring an array,
+# you first mention the array function followed by
+# type of number followed by a list of numbers to be stored"
+num = array("i", [688, 890, 789, 564, 7865, 3426, 6787])
+print(num)
+
+for x in num:
+    print(x)
+
