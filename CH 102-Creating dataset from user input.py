13074f210c0974514d5035250f5f3773e5167978
symonkipkemei <symonkipkemei@gmail.com>
2021-07-21 00:29:54 +0300

    The new world of dictionaries
    It is an interesting discovery.Most importantly, as a means of retrieving data easily.There is a thin delienation between lists and dictionaries.These edges seem to disappear when loop, an iterating statement, comes in.

--- CH 102-Creating dataset from user input.py
+++ CH 102-Creating dataset from user input.py
@@ -15,4 +15,37 @@ for y in range(0, 4):
     # this line of code updates the dict with a new pair of key and data
 
 print(listA)
+userName = input("enter name of one person in the list: ")
+print(listA[userName])
+print(listA)
 
+'''
+103
+Adapt program 102
+to display the
+names and ages of
+all the people in
+the list but do not
+show their shoe
+size.
+'''
+
+for y in listA:
+    print(y, listA[y]["age"])
+
+
+'''
+104
+After gathering the four names, ages and shoe sizes, ask the
+user to enter the name of the person they want to remove from
+the list. Delete this row from the data and display the other rows
+on separate lines
+'''
+
+removeName = input("input the name you want removed:")
+
+
+del listA[removeName]
+
+for x in listA:
+    print(x, listA[x]["age"], listA[x]["shoeSize"])
