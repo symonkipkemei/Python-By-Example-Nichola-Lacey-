eb0315c6a7d78f2778f211b43f432b907310a821
symonkipkemei <symonkipkemei@gmail.com>
2021-07-22 11:51:00 +0300

    writing to text files
    text files are critical, you need to be keen on opening ,closing and file mode of files

--- CH 105-110 text file.py
+++ CH 105-110 text file.py
@@ -142,3 +142,32 @@ elif selection == 3:
 
 else:
     print("You entered a wrong input")
+
+
+'''
+110
+Using the Names.txt file you
+created earlier, display the list of
+names in Python. Ask the user to
+type in one of the names and then
+save all the names except the one
+they entered into a new file called
+Names2.txt
+'''
+
+names = open("Names.txt", "r")
+print(names.read())
+names.close()
+
+names = open("Names.txt", "r")
+userType = input("Type one of the names:")
+userType = userType + "\n"
+for x in names:
+    if x != userType:
+        f = open("Names2.txt", "a")
+        f.write(x + "\n")
+        f.close()
+
+names = open("Names2.txt", "r")
+print(names.read())
+names.close()
