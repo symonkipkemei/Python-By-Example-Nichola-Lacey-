2e6ff082233777019829ad85e04cffdb3055c619
symonkipkemei <symonkipkemei@gmail.com>
2021-07-24 23:27:28 +0300

    understanding csv reader
     csv reader is a gem, it converts each row into an iterable, therfore, it we can retrieve any file .

--- understanding csv.py
+++ understanding csv.py
@@ -40,8 +40,54 @@ file.close()
 file = open("testing.csv", "r")
 for y in file:
     print(y)
+file.close()
+
+
+# EXPLORING THE ATTRIBUTE CSV READER
+# we have already understood how to write a string and variables to a a csv file
+# let us now evplore how to read a csv file
+
+file = open("testing.csv", "r")
+reader = csv.reader(file)
+for row in reader:
+    print(row)
+file.close()
+
+
+# why csv reader and not  file.read?
+
+# the difference is
+# file.read will just print each individual row separated by  commas
+# csv reader converts each row into an iterable , specifically a list.
+# Therefore we can access each column individually!
+
+#   ALTERNATIVELY
+
+# instead of reading a file with loop we can convert it to a list  using a list constructor
+
+file = open("testing.csv", "r")
+reader = csv.reader(file)
+row = list(reader)
+print(row)
+# Bombooclat!
+# listing the iterable converts it into a 2d list
+print(row[0])
+print(row[0][1])
+file.close()
 
 
 
+# SEARCHING FOR A FILE INA CSV
+
+# let's open the file first
+file = open("testing.csv", "r")
+#input your search file
+searchItem = input("Insert an item you would like to search?: ")
+
+# list all rows and seach if item is in the rows
+# This does not need a csv reader to convert it into an iterable
+for row in file:
+    if searchItem in row:
+        print(row)
 
 
