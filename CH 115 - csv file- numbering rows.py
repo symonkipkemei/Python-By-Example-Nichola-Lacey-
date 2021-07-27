e8a9c7224af9bca444da81e9b6d5f51cc2e0a1fe
symonkipkemei <symonkipkemei@gmail.com>
2021-07-27 22:06:47 +0300

    enumerating each row in a csv file

---
+++ CH 115 - csv file- numbering rows.py
@@ -0,0 +1,12 @@
+# we will try to use loop through a iterable csv and enumerate each row
+import csv
+file = open("Books.csv", "r")
+# converting csv into an iterable
+reader = csv.reader(file)
+# listing the iterables
+books = list(reader)
+
+for index, chapter in enumerate(books):
+    print("No", index, chapter)
+
+
