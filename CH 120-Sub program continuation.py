33ec219156a147bc5eaae6864c8fc54c0ed090b6
symonkipkemei <symonkipkemei@gmail.com>
2021-07-30 20:42:46 +0300

    sub-programs
    My own understanding of different types of sub-programs

---
+++ CH 120-Sub program continuation.py
@@ -0,0 +1,8 @@
+# Just a reminder
+# we have different categories of su programs as per my own classification.
+# They are:
+# 1. Go getters.
+# 2. Consumers that depend on Go Getters
+# 3. The compilers whom provides a platform for exchange of information between the Go getter and the Consumers
+
+
