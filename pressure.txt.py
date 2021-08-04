ee2bfbc9d198e8f3555eed2f94efebdc01f45622
symonkipkemei <symonkipkemei@gmail.com>
2021-08-04 23:39:10 +0300

    studiopressure
     Things are getting thicker,but when they do, the tougher keep going

---
+++ pressure.txt.py
@@ -0,0 +1,9 @@
+file = open("studiopressure.txt", "w")
+content = "Architectural studio is building up slowly but by God\n" \
+          " I am looking for strategies on how I can work out effectively\n" \
+          "and I have realised that my worst enemy is overthinking and fear\n" \
+          "maybe it is time we close one project at  a time\n" \
+          "then from the other week we can start polishing\n"
+
+file.write(content)
+file.close
