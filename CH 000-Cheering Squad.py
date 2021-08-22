43744cbe91c17c505647282de8301afdba7c7379
symonkipkemei <symonkipkemei@gmail.com>
2021-08-22 23:52:58 +0300

    Cheering Squad
    The high and lows of learning to code

---
+++ CH 000-Cheering Squad.py
@@ -0,0 +1,52 @@
+"""THE HIGHS AND LOWS OF LEARNING TO CODE UNDER NICHOLA LACEY'S PYTHON BY EXAMPLE"""
+
+# ARCHITECTURE PIN UP PREP(SIEGE DAY 1)
+
+# Architectural studio is building up slowly but by God's Grace I will pull through
+# I am looking for strategies on how I can work out effectively
+# and I have realised that my worst enemy is overthinking and fear
+# maybe it is time we close one project at  a time
+# then from the other week we can start polishing
+
+# ARCHITECTURE PIN UP PREP(SIEGE DAY 2)
+# Day two planning to complete details and other necessary elements in my design.
+# Tomorrow I will complete model for render and complete model for render and also
+
+
+# ARCHITECTURE PIN UP PREP(SIEGE DAY 3)
+# planning to start cleaning drawings
+# all other things will be frozen...SAT,SUN,MON.
+
+# ARCHITECTURE PIN UP PREP(SIEGE DAY 4)
+# Completed leather workshop.Should complete plans and modelling for render.
+
+# ARCHITECTURE PIN UP PREP(SIEGE DAY 5)
+# Forgot to commit on time.I have been extremely busy
+# Almost done with studio pushing up and out
+
+
+# ARCHITECTURE PIN UP PREP(SIEGE DAY 6)
+# Things are getting tough by the day .But the tougher it becomes , the more tough I become.
+
+
+# ARCHITECTURE PIN UP PREP(SIEGE DAY ...UNCOUNTED DAYS)
+# Too much to handle...Suspended coding for some moment
+
+
+# GLORY TO GOD!!
+# Completed architectural pin up successfully .
+# Free at last to pursue beyond and above the realms of built forms.
+
+# AAS 001
+# Redoing csv files study and subprograms just to remind myself how good or bad I am at
+# grabbing necessary components .I hope it will be great.Looking forward to  more engagements
+# This period will be called AAS,after architecture school.
+
+
+# AAS 002
+# Completed a recap of csv and subprograms
+# it was great to remind self of the different concepts that I had gathered along
+# I am impressed by how good I am at problem solving
+# This is really fascinating
+# I am good judge, a funny one so to say....
+# Looking forward to some GUI...This will be great!
